bbf73e61383c7a96d9a120825ff63179e99ac353
lucalin <luca.lin@tpinformation.com.tw>
2020-05-22 14:19:57 +0800

    code update

--- app/src/main/java/com/example/r30_a/recyclerviewpoc/helper/UpdateHelper.java
+++ app/src/main/java/com/example/r30_a/recyclerviewpoc/helper/UpdateHelper.java
@@ -2,10 +2,21 @@ package com.example.r30_a.recyclerviewpoc.helper;
 
 import android.content.ContentResolver;
 import android.content.ContentValues;
-import android.net.Uri;
-import android.provider.ContactsContract;
-import android.provider.ContactsContract.CommonDataKinds;
-import android.provider.ContactsContract.Data;
+import static android.provider.ContactsContract.Data;
+import static android.provider.ContactsContract.Data.*;
+import android.provider.ContactsContract.CommonDataKinds.StructuredName;
+import static android.provider.ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME;
+import android.provider.ContactsContract.CommonDataKinds.Phone;
+import static android.provider.ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE;
+import static android.provider.ContactsContract.CommonDataKinds.Phone.TYPE;
+import static android.provider.ContactsContract.CommonDataKinds.Phone.NUMBER;
+import android.provider.ContactsContract.CommonDataKinds.Note;
+import static android.provider.ContactsContract.CommonDataKinds.StructuredPostal;
+import static android.provider.ContactsContract.CommonDataKinds.StructuredPostal.*;
+import android.provider.ContactsContract.CommonDataKinds.Photo;
+import android.provider.ContactsContract.Contacts;
+import android.provider.ContactsContract.RawContacts;
+import android.provider.ContactsContract.CommonDataKinds.Email;
 /**
  * Created by R30-A on 2019/2/12.
  */
@@ -15,9 +26,9 @@ public class UpdateHelper {
     //新增名字
     public static boolean insertName(ContentResolver resolver, long contactId, String name){
         ContentValues values = new ContentValues();
-        values.put(Data.RAW_CONTACT_ID, contactId );
-        values.put(Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE);
-        values.put(CommonDataKinds.StructuredName.GIVEN_NAME, name);
+        values.put(RAW_CONTACT_ID, contactId );
+        values.put(MIMETYPE, StructuredName.CONTENT_ITEM_TYPE);
+        values.put(GIVEN_NAME, name);
         resolver.insert(Data.CONTENT_URI,values);
         return true;
     }
@@ -26,59 +37,56 @@ public class UpdateHelper {
     public static boolean insertPhoneNum(ContentResolver resolver, long contactId, String phoneNum) {
 
         ContentValues values = new ContentValues();
-        values.put(ContactsContract.Data.RAW_CONTACT_ID, contactId);
-        values.put(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE);
-        values.put(ContactsContract.CommonDataKinds.Phone.TYPE, ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE);
-        values.put(ContactsContract.CommonDataKinds.Phone.NUMBER, phoneNum);
-        resolver.insert(ContactsContract.Data.CONTENT_URI, values);
+        values.put(RAW_CONTACT_ID, contactId);
+        values.put(MIMETYPE, Phone.CONTENT_ITEM_TYPE);
+        values.put(TYPE, TYPE_MOBILE);
+        values.put(NUMBER, phoneNum);
+        resolver.insert(Data.CONTENT_URI, values);
 
         return true;
     }
     //新增備註
     public static void insertNote(ContentResolver resolver,String note,long contactId){
         ContentValues values = new ContentValues();
-        values.put(ContactsContract.CommonDataKinds.Note.NOTE, note);
-        values.put(ContactsContract.Data.RAW_CONTACT_ID, contactId);
-        values.put(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Note.CONTENT_ITEM_TYPE);
-        resolver.insert(ContactsContract.Data.CONTENT_URI, values);
+        values.put(Note.NOTE, note);
+        values.put(RAW_CONTACT_ID, contactId);
+        values.put(MIMETYPE, Note.CONTENT_ITEM_TYPE);
+        resolver.insert(Data.CONTENT_URI, values);
     }
     //新增地址
     public static void insertAddress(ContentResolver resolver,String city,String street,long contactId){
         ContentValues values = new ContentValues();
-        values.put(ContactsContract.CommonDataKinds.StructuredPostal.CITY, city);
-        values.put(ContactsContract.CommonDataKinds.StructuredPostal.STREET, street);
-        values.put(ContactsContract.Data.RAW_CONTACT_ID, contactId);
-        values.put(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredPostal.CONTENT_ITEM_TYPE);
-        resolver.insert(ContactsContract.Data.CONTENT_URI, values);
+        values.put(CITY, city);
+        values.put(STREET, street);
+        values.put(RAW_CONTACT_ID, contactId);
+        values.put(MIMETYPE, StructuredPostal.CONTENT_ITEM_TYPE);
+        resolver.insert(Data.CONTENT_URI, values);
     }
     //新增大頭貼
     public static void insertAvatar(ContentResolver resolver, String raw_contact_id,byte[] img_avatar_bytes){
         ContentValues values = new ContentValues();
-        values.put(ContactsContract.Data.RAW_CONTACT_ID, raw_contact_id);
-        values.put(ContactsContract.CommonDataKinds.Photo.PHOTO, img_avatar_bytes);
-        values.put(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Photo.CONTENT_ITEM_TYPE);
-        resolver.insert(ContactsContract.Data.CONTENT_URI, values);
+        values.put(RAW_CONTACT_ID, raw_contact_id);
+        values.put(Photo.PHOTO, img_avatar_bytes);
+        values.put(MIMETYPE, Photo.CONTENT_ITEM_TYPE);
+        resolver.insert(Data.CONTENT_URI, values);
     }
 
 
     //更新電話
     public static void updatePhone(ContentResolver resolver, String updatePhone, String raw_contact_id) {
         ContentValues values = new ContentValues();
-        values.put(ContactsContract.CommonDataKinds.Phone.NUMBER, updatePhone);
-        values.put(ContactsContract.CommonDataKinds.Phone.TYPE, ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE);
-        resolver.update(ContactsContract.Data.CONTENT_URI, values,
-                ContactsContract.Data.RAW_CONTACT_ID + " =?" + " AND " + ContactsContract.Data.MIMETYPE + " =?",
-                new String[]{raw_contact_id, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE});
+        values.put(NUMBER, updatePhone);
+        values.put(TYPE, TYPE_MOBILE);
+        resolver.update(Data.CONTENT_URI, values,
+                RAW_CONTACT_ID + " =?" + " AND " + MIMETYPE + " =?",
+                new String[]{raw_contact_id, Phone.CONTENT_ITEM_TYPE});
     }
 
     //更新名字
     public static void updateName(ContentResolver resolver, String updateName, String contact_id) {
         ContentValues values = new ContentValues();
-        values.put(ContactsContract.Contacts.DISPLAY_NAME, updateName);
-        resolver.update(
-                ContactsContract.RawContacts.CONTENT_URI,
-                values, ContactsContract.Data.CONTACT_ID + " =?",
-                new String[]{contact_id});
+        values.put(Contacts.DISPLAY_NAME, updateName);
+        resolver.update(RawContacts.CONTENT_URI, values, CONTACT_ID + " =?", new String[]{contact_id});
 
     }
 
@@ -87,11 +95,11 @@ public class UpdateHelper {
 
         if (!updateNote.equals(oldNote)) {
             ContentValues values = new ContentValues();
-            values.put(ContactsContract.CommonDataKinds.Note.NOTE, updateNote);
-            values.put(ContactsContract.Data._ID, contact_id);
-            values.put(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Note.CONTENT_ITEM_TYPE);
-            resolver.update(ContactsContract.Data.CONTENT_URI, values,
-                    ContactsContract.Data.CONTACT_ID + "=?" + " AND " + ContactsContract.Data.MIMETYPE + "='" + ContactsContract.CommonDataKinds.Note.CONTENT_ITEM_TYPE + "'"
+            values.put(Note.NOTE, updateNote);
+            values.put(_ID, contact_id);
+            values.put(MIMETYPE, Note.CONTENT_ITEM_TYPE);
+            resolver.update(Data.CONTENT_URI, values,
+                    CONTACT_ID + "=?" + " AND " + MIMETYPE + "='" + Note.CONTENT_ITEM_TYPE + "'"
                     , new String[]{contact_id});
         }
     }
@@ -99,27 +107,27 @@ public class UpdateHelper {
     //更新地址
     public static void updateAddress(ContentResolver resolver, String updateCity, String updateStreet, String contact_id, String raw_contact_id) {
         ContentValues values = new ContentValues();
-        values.put(ContactsContract.CommonDataKinds.StructuredPostal.CITY, updateCity);
-        values.put(ContactsContract.CommonDataKinds.StructuredPostal.STREET, updateStreet);
-        values.put(ContactsContract.CommonDataKinds.Phone.CONTACT_ID, contact_id);
-        values.put(ContactsContract.Data.MIMETYPE, "vnd.android.cursor.item/postal-address_v2");
+        values.put(CITY, updateCity);
+        values.put(STREET, updateStreet);
+        values.put(CONTACT_ID, contact_id);
+        values.put(MIMETYPE, "vnd.android.cursor.item/postal-address_v2");
 
-        resolver.update(ContactsContract.Data.CONTENT_URI, values,
-                ContactsContract.Data.RAW_CONTACT_ID + " =? AND "
-                        + ContactsContract.Data.MIMETYPE + "='vnd.android.cursor.item/postal-address_v2'",
+        resolver.update(Data.CONTENT_URI, values,
+                RAW_CONTACT_ID + " =? AND "
+                        + MIMETYPE + "='vnd.android.cursor.item/postal-address_v2'",
                 new String[]{raw_contact_id});
     }
 
     public static void updateEmail(ContentResolver resolver, String contact_id, String updateEmail_home, String type) {
         ContentValues values = new ContentValues();
-        values.put(ContactsContract.CommonDataKinds.Email.DATA, updateEmail_home);
-        values.put(ContactsContract.Data._ID, contact_id);
-        resolver.update(ContactsContract.Data.CONTENT_URI,
+        values.put(Email.DATA, updateEmail_home);
+        values.put(_ID, contact_id);
+        resolver.update(Data.CONTENT_URI,
                 values,
-                ContactsContract.CommonDataKinds.Email.CONTACT_ID + "=?" + " AND "
-                        + ContactsContract.CommonDataKinds.Email.MIMETYPE + "=?" + " AND "
-                        + ContactsContract.CommonDataKinds.Email.TYPE + "=?",
-                new String[]{contact_id, ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE, type});
+                Email.CONTACT_ID + "=?" + " AND "
+                        + Email.MIMETYPE + "=?" + " AND "
+                        + Email.TYPE + "=?",
+                new String[]{contact_id, Email.CONTENT_ITEM_TYPE, type});
 
     }
 }
